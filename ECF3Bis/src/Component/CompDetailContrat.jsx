327e55a9fe6499b258cad28aaf7cc99a9047944c
Administrateur <mathilde.bettembourg@gmail.com>
2023-01-12 14:37:12 +0100

    affichage details location reglee

--- ECF3Bis/src/Component/CompDetailContrat.jsx
+++ ECF3Bis/src/Component/CompDetailContrat.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { service } from "../Service/Service";
 import Card from 'react-bootstrap/Card';
 import { Link, Outlet } from "react-router-dom";
 import { serviceVehicules } from "../Service/ServiceVehicules";
 import { serviceContrats } from "../Service/ServiceContrats";
 import ListGroup from 'react-bootstrap/ListGroup';
+import { service } from "../Service/Service";
 
 export const CompDetailContrat = (props) => {
     /**
@@ -15,6 +15,8 @@ export const CompDetailContrat = (props) => {
     const params = useParams();
 
     const [detailContrat, setDetailContrat] = useState({});
+    const[locataire, setLocataire] = useState({})
+    const[vehicule, setVehicule] = useState({})
 
     /**
      * le useEffect ici va, à l'initialisation du composant, 
@@ -22,35 +24,41 @@ export const CompDetailContrat = (props) => {
      * en return il va setter le state de detail vehicule qui sera utilisé pour l'affichage.
      */
     useEffect(() => {
-        serviceContrats.seeContratsById(params.id).then((res) => setDetailContrat((res)))
+        serviceContrats.seeContratsById(params.id).then((res) => setDetailContrat(res))
     }, [])
+    useEffect(()=>{
+       setLocataire(detailContrat.locataire)
+        setVehicule(detailContrat.vehicule)
+    }, [detailContrat])
 
     return (
-        <>
-            <Card>
-                <Card.Header>{detailContrat.type}</Card.Header>
-                <Card.Body>
-                    <blockquote className="blockquote mb-0">
-                        <Card.Title>Locataire</Card.Title>
-                        <Card.Text> Identifiant du contrat: {detailContrat.id}</Card.Text>
-                        <hr/>
-                        <Card.Title>Renseignements</Card.Title>
-                        <Card.Text> Prenom : {detailContrat.prenom}</Card.Text>
-                        <Card.Text> Prenom : {detailContrat.nom}</Card.Text>
-                        <Card.Text> Email: {detailContrat.email}</Card.Text>
-                        <Card.Text> telephone : {detailContrat.telephone}</Card.Text>
-                       <hr/>
-                       <Card.Title>Vehicule Choisis</Card.Title>
-                        <Card.Text> Type de véhicule : {detailContrat.type}</Card.Text>
-                        <Card.Text> Prix à la journée : {detailContrat.prix}</Card.Text>
-                        <Card.Text> Marque du véhicule : {detailContrat.marque}</Card.Text>
-                        <Card.Text> Prix de la location : {detailContrat.prix} €</Card.Text>
-                        <Card.Text> Durée de la location : {detailContrat.duree} jours</Card.Text>
 
-                    </blockquote>
-                </Card.Body>
-            </Card>
-        </>
+         <>{detailContrat && locataire ?
+        
+             <Card>
+                 <Card.Header>{locataire.nom} - {vehicule.marque}</Card.Header>
+                 <Card.Body>
+                     <blockquote className="blockquote mb-0">
+                         <Card.Title>Locataire</Card.Title>
+                         <hr/>
+                         <Card.Title>Renseignements</Card.Title>
+                         <Card.Text> Prenom : {locataire.prenom}</Card.Text>
+                         <Card.Text> Prenom : {locataire.nom}</Card.Text>
+                         <Card.Text> Email: {locataire.email}</Card.Text>
+                         <Card.Text> Telephone : {locataire.telephone}</Card.Text>
+                        <hr/>
+                        <Card.Title>Vehicule Choisis</Card.Title>
+                         <Card.Text> Type de véhicule : {vehicule.type}</Card.Text>
+                         <Card.Text> Prix à la journée : {vehicule.prix}</Card.Text>
+                         <Card.Text> Marque du véhicule : {vehicule.marque}</Card.Text>
+                         <Card.Text> Prix de la location : {detailContrat.prix} €</Card.Text>
+                         <Card.Text> Durée de la location : {detailContrat.duree} jours</Card.Text>
+        
+                     </blockquote>
+                 </Card.Body>
+             </Card>:<></>
+         }
+         </>
     )
 }
 export default CompDetailContrat;
